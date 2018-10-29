b4241641e7d50271c18c4c0eb19f19937508623f
joshua <jojamsab12345@gmail.com>
2018-10-29 23:13:13 +0800

    First commit

---
+++ crawl.php
@@ -0,0 +1,33 @@
+<?php 
+include("classes/DomDocumentParser.php");
+	function createLink($src, $url) {
+		$scheme = parse_url($url)["scheme"];
+		$host = parse_url($url)["host"];
+		if(substr($src, 0, 2) == "//"){
+			$src = $scheme . ":" . $src;
+		} else if(substr($src, 0, 1) == "/") {
+			$src = $scheme . "://" . $host . $src;
+		} else if(substr($src, 0, 2) == "./") {
+			$src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+		} else if(substr($src, 0, 3) == "../") {
+			$src = $scheme . "://" . $host . "/" . $src;
+		}
+		return $src;
+	}
+	function followLinks($url) {
+		$parser = new DomDocumentParser($url);
+		$linkList = $parser->getLinks();
+		foreach($linkList as $link) {
+			$href = $link->getAttribute("href");
+			if(strpos($href, "#") !== false) {
+				continue;
+			} else if(substr($href, 0 , 11) == "javascript:") {
+				continue;
+			}
+			$href = createLink($href, $url);
+			echo $href . "<br>";
+		}
+	}
+	$startUrl = "http://www.bbc.com";
+	followLinks($startUrl);
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+ </head>
+ <body>
+ 	<div class="wrapper indexPage">
+ 		<div class="mainSection">
+	 		<div class="logoContainer">
+	 			<img src="assets/images/logo.png">
+	 		</div>
+	 		<div class="searchContainer">
+	 			<form action="search.php" method="GET">
+	 				<input type="text" class="searchBox" name="term">
+	 				<input type="submit" class="searchButton" value="Search">
+	 			</form>
+	 		</div>
+	 	</div>
+ 	</div>
+ </body>
+ </html>
